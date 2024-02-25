e1735c8ac5775ea33faa215514196f89dded1b76
lzw <lzw@gmail.com>
2024-02-26 00:48:56 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>home</h1>
+    <router-view></router-view>
+    <router-link to="/home">首页</router-link>
+    <router-link to="/favor">收藏</router-link>
+    <router-link to="/order">订单</router-link>
+    <router-link to="/message">消息</router-link>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="less" scoped>
+
+</style>
